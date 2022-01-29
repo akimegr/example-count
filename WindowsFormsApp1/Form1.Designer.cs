b983e4d2af2c66904a359fc7e6d731c30ef23c9d
akimegr <82463845+akimegr@users.noreply.github.com>
2022-01-29 17:39:41 +0300

    feat: add files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.field_x = new System.Windows.Forms.TextBox();
+            this.field_y = new System.Windows.Forms.TextBox();
+            this.field_z = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(325, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // field_x
+            // 
+            this.field_x.Location = new System.Drawing.Point(407, 24);
+            this.field_x.Name = "field_x";
+            this.field_x.Size = new System.Drawing.Size(157, 22);
+            this.field_x.TabIndex = 1;
+            this.field_x.Text = "1";
+            this.field_x.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // field_y
+            // 
+            this.field_y.Location = new System.Drawing.Point(407, 73);
+            this.field_y.Name = "field_y";
+            this.field_y.Size = new System.Drawing.Size(157, 22);
+            this.field_y.TabIndex = 2;
+            this.field_y.Text = "2";
+            this.field_y.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // field_z
+            // 
+            this.field_z.Location = new System.Drawing.Point(407, 121);
+            this.field_z.Name = "field_z";
+            this.field_z.Size = new System.Drawing.Size(157, 22);
+            this.field_z.TabIndex = 3;
+            this.field_z.Text = "3";
+            this.field_z.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(211, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите значение X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(211, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите значение Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(211, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите значение Z:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 198);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.field_z);
+            this.Controls.Add(this.field_y);
+            this.Controls.Add(this.field_x);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox field_x;
+        private System.Windows.Forms.TextBox field_y;
+        private System.Windows.Forms.TextBox field_z;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Лаб. раб N1 Ст.гр 10701320 ";
+
+            this.textBox1.Width = 265;
+            this.textBox1.Height = 100;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double x = Double.Parse(field_x.Text);
+                double y = Double.Parse(field_y.Text);
+                double z = Double.Parse(field_z.Text);
+                double S = Math.Pow((8 + Math.Pow(Math.Abs(x - y), 2) + 1), (1 / 3)) / (Math.Pow(x, 2) + Math.Pow(y, 2) + 2) - Math.Pow(Math.E, Math.Abs(x - y)) * (Math.Pow(Math.Pow(Math.Tan(z), 2) + 1, x));
+                string s = "";
+                s += " Лаб.раб N1 Ст.гр 10701320 Акимов Е.С. \r\n" ;
+                s += " x:=" + x.ToString()+"\r\n";
+                s += " y:= " + y.ToString() + "\r\n";
+                s += " z:= " + z.ToString() + "\r\n";
+                s += " Результат U:= " + S.ToString() + "\r\n";
+
+                textBox1.Text = s;
+            }
+            catch (FormatException ex)
+            {
+                double x = 1;
+                double y = 2;
+                double z = 3;
+
+                double S = Math.Pow((8 + Math.Pow(Math.Abs(x - y), 2) + 1), (1 / 3)) / (Math.Pow(x, 2) + Math.Pow(y, 2) + 2) - Math.Pow(Math.E, Math.Abs(x - y)) * (Math.Pow(Math.Pow(Math.Tan(z), 2) + 1, x));
+                string s = "";
+                s += " Лаб.раб N1 Ст.гр 10701320 Акимов Е.С. \r\n";
+                s += " ОШИБКА!!! Значения по умолчнаию: \r\n";
+                s += " x:=" + x.ToString() + "\r\n";
+                s += " y:= " + y.ToString() + "\r\n";
+                s += " z:= " + z.ToString() + "\r\n";
+                s += " Результат U:= " + S.ToString() + "\r\n";
+                textBox1.Text = s;
+            }
+
+            
+        }
+
+
+    }
+}
